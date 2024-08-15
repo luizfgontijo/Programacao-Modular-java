4b03da7a22e1e6add0ac2608f2ea7617e25895d5
Luiz Gontijo <lfgsoares05@gmail.com>
2024-08-15 11:43:23 -0300

    primeiro-commit

---
+++ helloworld/src/main/java/com/lulugolteste/helloworld/Helloworld.java
@@ -0,0 +1,22 @@
+package com.lulugolteste.helloworld;
+import java.util.Scanner;
+public class Helloworld {
+
+    public static void main(String[] args) {
+        sum();
+        /*
+        This function is made for calculate the sum of two numbers.
+        */
+        
+    }
+    static void sum(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite o priimeiro valor: ");
+        int value1 = s.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        int value2 = s.nextInt();
+        int finalvalue;
+        finalvalue = value1+value2;
+        System.out.println("Valor final "+finalvalue);
+    }
+}
