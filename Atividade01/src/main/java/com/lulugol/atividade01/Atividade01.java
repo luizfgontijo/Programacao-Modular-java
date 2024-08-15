0131670dd4ec05c532e6819a96aad4719d3c8500
Luiz Gontijo <lfgsoares05@gmail.com>
2024-08-15 11:49:16 -0300

    criação da pasta "Atividade01"

---
+++ Atividade01/src/main/java/com/lulugol/atividade01/Atividade01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lulugol.atividade01;
+
+/**
+ *
+ * @author lfgso
+ */
+public class Atividade01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
