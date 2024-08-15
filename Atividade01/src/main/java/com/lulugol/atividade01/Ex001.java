384044f1e3b8d3a681b87587c2be1b96a2ff6e65
Luiz Gontijo <lfgsoares05@gmail.com>
2024-08-15 11:52:36 -0300

    test

--- Atividade01/src/main/java/com/lulugol/atividade01/Ex001.java
+++ Atividade01/src/main/java/com/lulugol/atividade01/Ex001.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.lulugol.atividade01;
-
-/**
- *
- * @author lfgso
- */
 public class Ex001 {
-    
+    public static void main (String [] args)
+    {
+        System.out.println("Hello World");
+    }
 }
