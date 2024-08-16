9d5238bc808f297d6cb63ea5d04c37b5504589f6
Luiz Gontijo <lfgsoares05@gmail.com>
2024-08-16 12:26:22 -0300

    descrição ex001

--- Atividade01/src/main/java/com/lulugol/atividade01/Atividade01.java
+++ Atividade01/src/main/java/com/lulugol/atividade01/Atividade01.java
@@ -1,3 +1,8 @@
+/*
+    Faça um programa que leia um vetor A com 15 elementos inteiros. Em seguida, construa
+um vetor B do mesmo tipo, em que cada elemento de B deva ser o resultado do fatorial
+correspondente de cada elemento de A. Imprima na tela os vetores A e B.
+*/
 package com.lulugol.atividade01;
 
 import java.util.Scanner;
