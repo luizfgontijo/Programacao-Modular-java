610d2fd6d5a70918757815bbe93fd070353ad059
Luiz Gontijo <lfgsoares05@gmail.com>
2024-08-16 12:23:38 -0300

    finalizei o ex001

--- Atividade01/src/main/java/com/lulugol/atividade01/Atividade01.java
+++ Atividade01/src/main/java/com/lulugol/atividade01/Atividade01.java
@@ -1,10 +1,31 @@
 package com.lulugol.atividade01;
 
-public class Atividade01 {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        /*
-        Criado para armazenar a atividade 1 de Progrmação modular.
-        */
+public class Atividade01 {
+    public static void main (String [] args)
+    {
+        Scanner s = new Scanner (System.in);
+        int vetor []= new int [5];
+        for (int i=0; i<5; i++)// alterar pra 15 depois dos testes
+        {
+            System.out.println("Digite o A["+ i +"]");
+            vetor [i] = s.nextInt();
+        }
+        System.out.println("");
+        int vetorFat[] = new int[5];
+        
+        for (int i = 0; i < 5; i++) {
+            int fat = 1;
+            for (int j = 1; j <= vetor[i]; j++) {
+                fat *= j;
+            }
+            vetorFat[i] = fat;
+        }
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Fatorial de " + vetor[i] + " = " + vetorFat[i]);
+        }
+        s.close();
     }
 }
--- Atividade01/src/main/java/com/lulugol/atividade01/Ex001.java
+++ Atividade01/src/main/java/com/lulugol/atividade01/Ex001.java
@@ -1,7 +1,36 @@
 package com.lulugol.atividade01;
+
+import java.util.Scanner;
+
 public class Ex001 {
     public static void main (String [] args)
     {
-        System.out.println("Hello World");
+        Scanner s = new Scanner (System.in);
+        int vetor []= new int [5];
+        for (int i=0; i<5; i++)// alterar pra 15 depois dos testes
+        {
+            System.out.println("Digite o A["+ i +"]");
+            vetor [i] = s.nextInt();
+        }
+        System.out.println("");
+        int vetorFat[] = new int[5];
+        int fat = 0;
+        for (int i =0;i <5; i++){
+            int n =1;
+            do {
+            fat = vetor [i];
+            n = fat; 
+            n= n -1;
+            fat = fat *n;
+            }
+            while (n>=1);
+            vetorFat[i] = fat;
+            
+        }
+        for ( int i =0; i <5; i++)
+        {
+            System.out.println(vetorFat[i]+"");
+        }
+        s.close();
     }
 }
