2dfe729235be7da62d3fc9fc29fe1a270193b00b
Luiz Gontijo <lfgsoares05@gmail.com>
2024-08-20 17:09:57 -0300

    commit

--- Atividade01/src/main/java/com/lulugol/atividade01/Ex007.java
+++ Atividade01/src/main/java/com/lulugol/atividade01/Ex007.java
@@ -3,42 +3,46 @@ package com.lulugol.atividade01;
 import java.util.Scanner;
 
 public class Ex007 {
-    public static int primeiroVetor (int r){
-        int vetor[] = new int [r];
-        Scanner sc= new Scanner (System.in);
-        for (int i=0; i <r; i++){
-            System.out.println("Digite o elemento "+ i+1);
-            vetor[i]= sc.nextInt();
-            sc.nextLine();
+    public static int[] primeiroVetor(int r) {
+        int[] vetor = new int[r];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < r; i++) {
+            System.out.println("Digite o elemento " + (i + 1) + ":");
+            vetor[i] = sc.nextInt();
         }
-        return vetor[r];
+        return vetor;
     }
-    public static int segundoVetor (int r){
-        int vetor[] = new int [r];
-        Scanner sc= new Scanner (System.in);
-        for (int i=0; i <r; i++){
-            System.out.println("Digite o elemento "+ i+1);
-            vetor[i]= sc.nextInt();
-            sc.nextLine();
+
+    public static int[] segundoVetor(int r) {
+        int[] vetor = new int[r];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < r; i++) {
+            System.out.println("Digite o elemento " + (i + 1) + ":");
+            vetor[i] = sc.nextInt();
         }
-        return vetor[r];
+        return vetor;
     }
-    public static int somaVetores (int r, int[] vetorX, int[] vetorY){
-        int vetor[] = new int[r];
-        
-        for (int i=0; i <r; i++){
-            vetor[i]=vetorX[i]+vetorY[i];
+
+    public static int[] somaVetores(int r, int[] vetorX, int[] vetorY) {
+        int[] vetor = new int[r];
+        for (int i = 0; i < r; i++) {
+            vetor[i] = vetorX[i] + vetorY[i];
         }
-        
-        return vetor[r];
+        return vetor;
     }
-    public static void main (String[]args){
-        Scanner s = new Scanner (System.in);
-        System.out.println("Qual sera o tamanho dos vetores? ");
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Qual será o tamanho dos vetores?");
         int n = s.nextInt();
-        s.nextLine();
-        int vetorA = primeiroVetor(n);
-        int vetorB = segundoVetor(n);
-        int vetorS = somaVetores(n, vetorA, vetorB);
+
+        int[] vetorA = primeiroVetor(n);
+        int[] vetorB = segundoVetor(n);
+        int[] vetorS = somaVetores(n, vetorA, vetorB);
+
+        System.out.println("Vetor final: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(vetorS[i]);
+        }
     }
 }
