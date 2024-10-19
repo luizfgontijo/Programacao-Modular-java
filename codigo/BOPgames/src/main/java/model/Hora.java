97e0dad3246f105d47d8e57374319cfcdb295606
Luiz Gontijo <lfgsoares05@gmail.com>
2024-10-19 16:30:18 -0300

    Add files via upload

---
+++ codigo/BOPgames/src/main/java/model/Hora.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Hora {
+    private int horas;
+    private int minutos;
+    private int segundos;
+
+    public Hora(int horas, int minutos, int segundos) {
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+
+    public String horaFormatada() {
+        return String.format("%02d:%02d:%02d", horas, minutos, segundos);
+    }
+}
+
